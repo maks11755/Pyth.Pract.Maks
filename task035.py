b5444e231214198395dc6dee29929dba18694cea
maksim <your>
2023-11-14 16:52:08 +1100

    Added task035

--- task035.py
+++ task035.py
@@ -2,12 +2,19 @@
 # Напоминание: Простое число - это число, которое имеет 2 делителя: 1  и n(само число)
 # Input: 5
 # Output: yes
+# def Prime(n):
+#     d = 2
+#     while n % d != 0:
+#         d += 1
+#     return d == n
 
- 
-def Prime(n):
-    d = 2
-    while n % d != 0:
-        d += 1
-    return d == n
+# print(Prime(5))
 
-print(Prime(4))
+def prime_num(n):
+    if n <= 1:
+        return "No"
+    for i in range(2, n // 2,):
+        if n % i == 0:
+            return 'no'
+        return ' yes'
+print(prime_num(5))   
