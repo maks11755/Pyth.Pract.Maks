6556f74582a0021c8152bada8596a9f948b8efab
maksim <your>
2023-11-29 16:23:40 +1100

    Added task036

--- task036.py
+++ task036.py
@@ -22,18 +22,33 @@
 # Шестая (6) строка - первая (1), умноженная на 6
 
 
-def print_operation_table(operation, n, m):
-    table = []
-    for i in range(1, n + 1):
-        line = []
-        for j in range(1, m + 1):
-            line.append(operation(i,j))
-        table.append(line)
+# def print_operation_table(operation, n, m):
+#     table = []
+#     for i in range(1, n + 1):
+#         line = []
+#         for j in range(1, m + 1):
+#             line.append(operation(i, j))
+#         table.append(line)
 
-    for row in table:
-        print(("{: <7}"*m).format(*row))
+#     for row in table:
+#         print(("{: <7}"*m).format(*row))
 
-    print("Искомый элемент> ", operation(n,m))
+#     print("Искомый элемент> ", operation(n,m))
 
-x, y = map(int, input("Введите 2 значения через пробел> ").split())
-print_operation_table(lambda x, y: x * y, x, y)
+# x, y = map(int, input("Введите 2 значения через пробел> ").split())
+# print_operation_table(lambda x, y: x * y, x, y)
+
+
+def print_operation_table(operation, num_rows, num_columns):
+    if num_rows < 2:
+        print('Error. Enter number > 2')
+    else:
+        for i in range(1, num_rows + 1):
+            if i == 1:
+                sq = [i for i in range(1, num_rows + 1)]
+            else:
+                sq = [operation(i, j) if j > 1 else i for j in range(1, num_columns + 1)]
+            print(*sq)
+
+
+print_operation_table(lambda x, y: x + y, 8, 8)
